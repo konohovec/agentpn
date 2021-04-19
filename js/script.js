7705473f975bffb958a7b4d00c91c39db93e38e1
konohovec <lizunova.252@gmail.com>
2021-04-19 15:19:51 +0300

    19.04

--- js/script.js
+++ js/script.js
@@ -1,7 +1,12 @@
 jQuery(document).ready(function($) {
 	$('.menu__icon').click(function(event) {
-		$('.menu__icon').toggleClass('_active');
+		$('.menu__icon, .header__list, .header__contacts_mobile, body').toggleClass('_active');
 	});
+	$('.header__link').click(function (){
+		if ($('.menu__icon, .header__list, .header__contacts_mobile, body').hasClass('_active')){
+			$('.menu__icon, .header__list, .header__contacts_mobile, body').removeClass("_active")
+		}
+	})
 });
 
 new Swiper ('.partners__content-slider', {
@@ -10,10 +15,16 @@ new Swiper ('.partners__content-slider', {
 		draggable: true,
 	},
 	autoHeight: true,
-	slidesPerView: 2,
+	slidesPerView: 3.5,
 	mousewheel: {
 		sensitivity: 1,
 		eventsTarget: '.partners__content-slider',
+	},
+	breakpoints: {
+		320: {
+			slidesPerView: 2,
+			spaceBetween: 8,
+		}
 	}
 });
 
@@ -23,9 +34,54 @@ new Swiper ('.news__slider', {
 		draggable: true,
 	},
 	spaceBetween: 20,
-	slidesPerView: 2,
+	slidesPerView: 2.5,
 	mousewheel: {
 		sensitivity: 1,
 		eventsTarget: '.news__slider',
+	},
+	breakpoints: {
+		320: {
+			slidesPerView: 2,
+			spaceBetween: 8,
+		}
 	}
 });
+
+new Swiper ('.achievement__slider', {
+	slidesPerView: 1,
+});
+
+new Swiper ('.info__slider', {
+	slidesPerView: 1,
+	autoHeight: true,
+	spaceBetween: 8,
+});
+
+let elementForm = document.querySelector('#form__wrapper');
+let elementCall = document.querySelector('#callback')
+let Visible = function (target1, target2) {
+	let targetPositionForm = {
+		top: window.pageYOffset + target1.getBoundingClientRect().top,
+	bottom: window.pageYOffset + target1.getBoundingClientRect().bottom
+	};
+	let targetPositionCall = {
+		top: window.pageYOffset + target2.getBoundingClientRect().top,
+		bottom: window.pageYOffset + target2.getBoundingClientRect().bottom
+	};
+	windowPosition = {
+		top: window.pageYOffset,
+		bottom: window.pageYOffset + document.documentElement.clientHeight
+	};
+
+	if (targetPositionForm.bottom > windowPosition.top && targetPositionForm.top < windowPosition.bottom || targetPositionCall.bottom > windowPosition.top && targetPositionCall.top < windowPosition.bottom) { // Если позиция левой стороны элемента меньше позиции правой чайти окна, то элемент виден справа
+		$('.partners__content-button').addClass('fixed__button');
+	} else {
+		$('.partners__content-button').removeClass('fixed__button');
+	};
+};
+
+window.addEventListener('scroll', function() {
+	Visible (elementForm, elementCall);
+});
+
+Visible (elementForm, elementCall);
